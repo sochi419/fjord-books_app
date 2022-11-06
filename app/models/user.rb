bb77458f5d4288fc5c487409e4fc4c157b569bd6
Soshi Ochiai <equipment2241@gmail.com>
2022-11-06 10:12:03 +0900

    Implemented user follow function.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
+  has_many :reverse_of_follows, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
+
+  has_many :followings, through: :follows, source: :followed
+  has_many :followers, through: :reverse_of_follows, source: :follower
+
+  def follow(user_id)
+    follows.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follows.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'follows/followings'
+  get 'follows/followers'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+
+  resources :users do
+    resource :follows, only: [:create, :destroy]
+    get 'followings' => 'follows#followings', as: 'followings'
+    get 'followers' => 'follows#followers', as: 'followers'
+  end
 end
