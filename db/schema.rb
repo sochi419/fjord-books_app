3a280fb9f14a47c5227e2297a0b3825076ebb6e7
sochi419 <equipment2241@gmail.com>
2022-12-13 11:46:15 +0900

    Removed unnecessary tables

--- db/schema.rb
+++ db/schema.rb
@@ -49,17 +49,6 @@ ActiveRecord::Schema.define(version: 2022_11_17_023639) do
     t.string "picture"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text "content", null: false
-    t.string "commentable_type", null: false
-    t.integer "commentable_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id", null: false
     t.integer "followed_id", null: false
@@ -77,16 +66,7 @@ ActiveRecord::Schema.define(version: 2022_11_17_023639) do
     t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
     t.index ["following_id"], name: "index_relationships_on_following_id"
   end
-
-  create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_reports_on_user_id"
-  end
-
+  
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
