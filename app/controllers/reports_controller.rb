61f13c493978f205b13853c881e529123a2b7b97
sochi419 <equipment2241@gmail.com>
2022-12-18 11:35:43 +0900

    report newする処理を1行に簡略化

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,9 +15,8 @@ class ReportsController < ApplicationController
 
   def edit; end
 
-  def create
-    @report = Report.new(report_params)
-    @report.user_id = current_user.id
+  def create    
+    @report = Report.new(title: report_params[:title], content: report_params[:content], user_id: current_user.id)
 
     if @report.save
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
